ab11359449bc109554845e72231f21683664fd32
marcobravociencias <marcobravo@ciencias.unam.mx>
2025-07-20 13:52:24 -0600

    Web Version 0.3.1

--- src/Components/LeftDecComponent.tsx
+++ src/Components/LeftDecComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CheckCircleIcon } from '@heroicons/react/20/solid'
 
-const benefits = [
+const benefits_ti = [
     'Creación de sitios web a medida, responsivos y optimizados para SEO.',
 	'Desarrollo de tiendas en línea con plataformas seguras y escalables.',
 	'Diseño y programación de software personalizado según tus necesidades.',
@@ -17,30 +17,21 @@ function LeftDecComponent() {
                     <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
                         <img
                             alt=""
-                            src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+                            src={require('../img/pic02_brain_side.jpg')}
                             className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
                         />
                         <div className="w-full flex-auto">
                             <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">
                                 Desarrollo y soporte TI
                             </h2>
-                            <p className="mt-6 text-lg/8 text-pretty text-gray-300">
-                                Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-                                accusamus quisquam.
-                            </p>
                             <ul role="list" className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base/7 text-white sm:grid-cols-2">
-                                {benefits.map((benefit) => (
+                                {benefits_ti.map((benefit) => (
                                     <li key={benefit} className="flex gap-x-3">
                                         <CheckCircleIcon aria-hidden="true" className="h-7 w-5 flex-none" />
                                         {benefit}
                                     </li>
                                 ))}
                             </ul>
-                            <div className="mt-10 flex">
-                                <a href="#" className="text-sm/6 font-semibold text-indigo-400">
-                                    See our job postings <span aria-hidden="true">&rarr;</span>
-                                </a>
-                            </div>
                         </div>
                     </div>
                 </div>
